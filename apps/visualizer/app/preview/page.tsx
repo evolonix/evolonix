08861efc5462a512710a64e3bb392bebf805f88a
Jason Ruesch <jruesch@degreed.com>
2024-02-03 22:40:28 -0500

    bak

---
+++ apps/visualizer/app/preview/page.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface PreviewProps {}
+
+export function Preview(props: PreviewProps) {
+  return (
+    <div>
+      <h1>Welcome to Preview!</h1>
+    </div>
+  );
+}
+
+export default Preview;
